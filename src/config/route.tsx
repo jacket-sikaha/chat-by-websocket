a9cb59f2f4aaddcdaed11ec2b214ffed6f829b6f
sikara <xuhaoxian@gdlinkjob.com>
2025-04-16 11:45:30 +0800

    docs 去除无用文件

--- src/config/route.tsx
+++ src/config/route.tsx
@@ -1,11 +1,9 @@
+import { Spin } from 'antd';
 import { Suspense, lazy } from 'react';
 import App from '../App.tsx';
 import Home from '../pages/home/index';
-// import Theme from "../pages/theme/index";
 
-// const Home = () => import("../pages/home.tsx");
 // react router6 + react lazy 延迟加载的正确写法
-const Theme = lazy(() => import('../pages/theme'));
 const Chat = lazy(() => import('../pages/index'));
 
 export const DefaultRoutes = [
@@ -18,23 +16,25 @@ export const DefaultRoutes = [
         name: '主页',
         element: <Home />
       },
-
-      {
-        path: 'theme',
-        name: '多种主题切换',
-        element: (
-          <Suspense fallback={<h1>loading</h1>}>
-            {/* 这里的Suspense就是只对Theme起作用 */}
-            <Theme />
-          </Suspense>
-        )
-      },
       {
         path: 'index',
         name: '主页',
         element: (
-          <Suspense fallback={<h1>loading</h1>}>
+          <Suspense
+            fallback={<Spin size="large" className="flex justify-center items-center h-screen" />}
+          >
             {/* 这里的Suspense就是只对Theme起作用 */}
+            <Chat />
+          </Suspense>
+        )
+      },
+      {
+        path: 'history',
+        name: '历史记录',
+        element: (
+          <Suspense
+            fallback={<Spin size="large" className="flex justify-center items-center h-screen" />}
+          >
             <Chat />
           </Suspense>
         )
