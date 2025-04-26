20b5e01329ab1f0be56a9c0619aa32d878cee032
sikara <xuhaoxian@gdlinkjob.com>
2025-04-26 11:56:17 +0800

    feat 添加卡片动画

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -8,7 +8,7 @@ function History() {
   const messages = useChatMessageStore.use.messages();
   messages;
   return (
-    <div className="mx-auto flex max-w-2xl flex-col items-center gap-3 p-3">
+    <div className="mx-auto flex h-full max-w-2xl flex-col items-center gap-3 p-3">
       {messages.map((item, index) => {
         const { type, file, from } = item;
         const k = ChatMsgType[type] as keyof typeof ChatMsgType;
@@ -17,41 +17,47 @@ function History() {
         return (
           <div
             key={index}
-            className="service-card group flex w-full shrink-0 cursor-pointer snap-start flex-col items-start gap-3 rounded-lg bg-white p-6 shadow-xl transition-all duration-300 hover:bg-[#006e25]"
+            className="service-card group relative w-full shrink-0 cursor-pointer snap-start overflow-hidden rounded-lg bg-[#fafafa] p-6 shadow-xl transition-all duration-300 hover:bg-[#07689f]"
           >
-            {Array.isArray(content) ? (
-              <div className="flex flex-wrap items-center gap-2">
-                {content?.map((item) => (
-                  <div key={item.uid}>
-                    <Attachments.FileCard item={item} />
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <Typography.Text copyable className="text-lg text-black/80 group-hover:text-white">
-                {content}
-              </Typography.Text>
-            )}
+            <span className="absolute right-0 top-0 z-0 size-10 transform rounded-full bg-gradient-to-r from-purple-500 to-blue-500 opacity-75 transition-all duration-500 group-hover:scale-[20]" />
+            <span className="absolute right-0 top-0 z-10 flex size-10 items-center justify-center text-lg text-white">
+              {index + 1}
+            </span>
+            <div className="z-10 flex flex-col items-start gap-3 px-2">
+              {Array.isArray(content) ? (
+                <div className="flex flex-wrap items-center gap-2 text-black/80 group-hover:text-white">
+                  {content?.map((item) => (
+                    <div key={item.uid}>
+                      <Attachments.FileCard item={item} />
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <Typography.Text copyable className="text-lg text-black/80 group-hover:text-white">
+                  {content}
+                </Typography.Text>
+              )}
 
-            <div className="flex w-full items-center justify-between">
-              <div>
-                {Array.isArray(content) && (
-                  <DownloadOutlined
-                    className="rounded-full bg-[#736DD3] p-1.5 text-lg text-black/80 group-hover:text-white"
-                    onClick={() => {
-                      Promise.all(
-                        content.map((file) => onDownload(from, file.fid, file.type, file.name))
-                      );
-                    }}
-                  />
-                )}
+              <div className="flex w-full items-center justify-between">
+                <div>
+                  {Array.isArray(content) && (
+                    <DownloadOutlined
+                      className="rounded-full bg-[#a2d5f2] p-1.5 text-lg text-white"
+                      onClick={() => {
+                        Promise.all(
+                          content.map((file) => onDownload(from, file.fid, file.type, file.name))
+                        );
+                      }}
+                    />
+                  )}
+                </div>
+                <p
+                  style={{ WebkitTextStroke: '1px #ff7e67', WebkitTextFillColor: 'transparent' }}
+                  className="z-10 self-end font-bold"
+                >
+                  <span className="italic">From: </span> {from}
+                </p>
               </div>
-              <p
-                style={{ WebkitTextStroke: '1px #A66216', WebkitTextFillColor: 'transparent' }}
-                className="self-end font-bold"
-              >
-                <span className="italic">From: </span> {from}
-              </p>
             </div>
           </div>
         );
